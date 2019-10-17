741e57e29e38b752e84037380133e19b5ca944fe
Nicolás <nicolas.fuentest@um.es>
2019-10-17 13:06:26 +0200

    Añadido menú

---
+++ FCV_Juego/Assets/Scripts/menu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+    /*[SerializeField]
+    GameObject panelAyuda;*/
+
+    public void Jugar(int i)
+    {
+        SceneManager.LoadScene(i);
+    }
+
+    /*public void Ayuda()
+    {
+        panelAyuda.SetActive(true);
+    }*/
+
+    public void Salir()
+    {
+        Application.Quit();
+    }
+
+    /*public void SalirAyuda()
+    {
+        panelAyuda.SetActive(false);
+    }*/
+}
